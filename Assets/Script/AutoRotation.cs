5def3bff08623d35c90b0ae6dde26ba3ffdd385d
turekj <turekj@spseplzen.cz>
2025-02-25 21:44:18 +0100

    museum

---
+++ Assets/Script/AutoRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotation : MonoBehaviour
+{
+    public float angularSpeed = 100.0f;
+    public float heading = 0.0f;
+
+    void start() {
+        heading = 0;        
+    }
+
+    void Update() {
+        heading += angularSpeed * Time.deltaTime;
+        this.transform.eulerAngles = new Vector3(this.transform.eulerAngles.x, heading, this.transform.eulerAngles.z);
+    }
+}
